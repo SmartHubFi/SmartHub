8b49cb05b9ab3932e8255cbaedbadc9956f42c7e
SmartHubFi <w4sh.wmv@gmail.com>
2024-05-08 20:22:09 -0300

    Implemented dynamic content loading and search functionality
    Added JavaScript functionality to dynamically load movie data from the movie.json file onto the website. Implemented event listeners for left and right navigation buttons to scroll through movie cards horizontally. Integrated search functionality to filter movies based on user input in the search bar, dynamically updating the displayed movie cards accordingly.

---
+++ app.js
@@ -0,0 +1,193 @@
+let left_btn = document.getElementsByClassName('bi-chevron-left')[0];
+let right_btn = document.getElementsByClassName('bi-chevron-right')[0];
+let cards = document.getElementsByClassName('cards')[0];
+let search = document.getElementsByClassName('search')[0];
+let search_input = document.getElementById('search_input');
+
+left_btn.addEventListener('click', () => {
+    cards.scrollLeft -= 140;
+})
+right_btn.addEventListener('click', () => {
+    cards.scrollLeft += 140;
+})
+
+let json_url = "movie.json";
+
+fetch(json_url).then(Response => Response.json())
+    .then((data) => {
+        data.forEach((ele, i) => {
+            let {name, imdb, date, sposter, bposter, genre, url} = ele;
+            let card = document.createElement('a');
+            card.classList.add('card');
+            card.href = url;
+            card.innerHTML = ` 
+                <img src="${sposter}" alt="" class="poster">
+                <div class="rest_card">
+                    <img src="${bposter}" alt="">
+                    <div class="cont">
+                        <h4>${name}</h4>
+                        <div class="sub">
+                            <p>${genre}, ${date}</p>
+                            <h3><span>IMDB</span><i class="bi bi-star-fill"></i> ${imdb}</h3>
+                        </div>
+                    </div>
+                </div>
+            `
+            cards.appendChild(card);
+        });
+
+        document.getElementById('title').innerText = data[0].name;
+        document.getElementById('gen').innerText = data[0].genre;
+        document.getElementById('date').innerText = data[0].date;
+        document.getElementById('rate').innerHTML = `<span>IMDB</span><i class="bi bi-star-fill"></i> ${data[0].imdb}`;
+
+        // search data Load
+        data.forEach(element => {
+            let { name, imdb, date, sposter, genre, url } = element;
+            let card = document.createElement('a');
+            card.classList.add('card');
+            card.href = url;
+            card.innerHTML = `
+            <img src="${sposter}" alt="">
+                        <div class="cont">
+                            <h3>${name} </h3>
+                            <p>${genre}, ${date}<span>IMDB </span><i class="bi bi-star-fill"></i> ${imdb}</p>
+                        </div>
+            `
+            search.appendChild(card);
+        });
+
+        //search filter
+
+        search_input.addEventListener('keyup', ()=> {
+            let filter = search_input.value.toUpperCase();
+            let a = search.getElementsByTagName('a');
+
+            for (let index = 0; index < a.length; index++) {
+                let b = a[index].getElementsByClassName('cont')[0];
+                // console.log(a.textContent)
+                let TextValue = b.textContent || b.innerText;
+                if (TextValue.toUpperCase().indexOf(filter) > -1) {
+                    a[index].style.display = "flex";
+                    search.style.visibility = "visible";
+                    search.style.opacity = 1;
+                } else {
+                    a[index].style.display = "none";
+                }
+                if (search_input.value == 0) {
+                    search.style.visibility = "hidden";
+                    search.style.opacity = 0;
+                }
+            }
+        });
+
+        let video = document.getElementsByTagName('video')[0];
+        let play = document.getElementById('play');
+        play.addEventListener('click', () => {
+            if (video.paused) {
+                video.play();
+                play.innerHTML = `Pausar <i class="bi bi-pause-fill"></i>`
+            } else {
+                video.pause();
+                play.innerHTML = `Assistir <i class="bi bi-play-fill"></i>`
+            }
+        });
+        let download_main = document.getElementById('download_main');
+        download_main.addEventListener('click', function(event){
+            event.preventDefault();
+            window.location.href = 'oppenheimer.html';
+        });
+
+        let Séries = document.getElementById('Séries');
+        let Filmes = document.getElementById('Filmes');
+        let Animes = document.getElementById('Animes');
+
+        Séries.addEventListener('click', ()=> {
+            cards.innerHTML = ''; 
+
+            let Séries_array = data.filter(ele => {
+                return ele.type == "Séries";
+            });
+
+            Séries_array.forEach((ele, i) => {
+                let {name, imdb, date, sposter, bposter, genre, url} = ele;
+                let card = document.createElement('a');
+                card.classList.add('card');
+                card.href = url;
+                card.innerHTML = ` 
+                    <img src="${sposter}" alt="" class="poster">
+                    <div class="rest_card">
+                        <img src="${bposter}" alt="">
+                        <div class="cont">
+                            <h4>${name}</h4>
+                            <div class="sub">
+                                <p>${genre}, ${date}</p>
+                                <h3><span>IMDB</span><i class="bi bi-star-fill"></i> ${imdb}</h3>
+                            </div>
+                        </div>
+                    </div>
+                `
+                cards.appendChild(card);
+            });
+
+
+        });
+        Filmes.addEventListener('click', ()=> {
+            cards.innerHTML = ''; 
+
+            let Filmes_array = data.filter(ele => {
+                return ele.type == "Filmes";
+            });
+
+            Filmes_array.forEach((ele, i) => {
+                let {name, imdb, date, sposter, bposter, genre, url} = ele;
+                let card = document.createElement('a');
+                card.classList.add('card');
+                card.href = url;
+                card.innerHTML = ` 
+                    <img src="${sposter}" alt="" class="poster">
+                    <div class="rest_card">
+                        <img src="${bposter}" alt="">
+                        <div class="cont">
+                            <h4>${name}</h4>
+                            <div class="sub">
+                                <p>${genre}, ${date}</p>
+                                <h3><span>IMDB</span><i class="bi bi-star-fill"></i> ${imdb}</h3>
+                            </div>
+                        </div>
+                    </div>
+                `
+                cards.appendChild(card);
+            });
+
+        })
+        Animes.addEventListener('click', ()=> {
+            cards.innerHTML = ''; 
+
+            let Animes_array = data.filter(ele => {
+                return ele.type == "Animes";
+            });
+
+            Animes_array.forEach((ele, i) => {
+                let {name, imdb, date, sposter, bposter, genre, url} = ele;
+                let card = document.createElement('a');
+                card.classList.add('card');
+                card.href = url;
+                card.innerHTML = ` 
+                    <img src="${sposter}" alt="" class="poster">
+                    <div class="rest_card">
+                        <img src="${bposter}" alt="">
+                        <div class="cont">
+                            <h4>${name}</h4>
+                            <div class="sub">
+                                <p>${genre}, ${date}</p>
+                                <h3><span>IMDB</span><i class="bi bi-star-fill"></i> ${imdb}</h3>
+                            </div>
+                        </div>
+                    </div>
+                `
+                cards.appendChild(card);
+            });
+        })
+
+    });
